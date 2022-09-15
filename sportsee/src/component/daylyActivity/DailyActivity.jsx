d1c3b370cc4863b914b12f3777d7df87f4470bce
JorisMLN <monleon.joris@gmail.com>
2022-09-15 15:27:54 +0200

    Add daily bar chart

--- sportsee/src/component/daylyActivity/DailyActivity.jsx
+++ sportsee/src/component/daylyActivity/DailyActivity.jsx
@@ -1,10 +1,25 @@
 import './activity.scss';
 
+import { ResponsiveContainer, BarChart, CartesianGrid, XAxis, YAxis, Bar, Tooltip, Legend } from 'recharts';
+import { dailyData } from '../../mock/dataMocked';
+
 const DailyActivity = () => {
+
+  const data = dailyData;
 
   return (
     <div className="dailyActivity">
-      DailyActivity
+      <ResponsiveContainer >
+        <BarChart width={730} height={250} data={data}>
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Bar dataKey="pv" fill="#8884d8" />
+          <Bar dataKey="uv" fill="#82ca9d" />
+        </BarChart>
+      </ResponsiveContainer>
     </div>
   )
 }
