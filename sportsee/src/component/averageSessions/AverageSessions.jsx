095bb2d3f208bd2fd40bba5b7f53ee5b94afec40
JorisMLN <monleon.joris@gmail.com>
2022-09-15 14:57:51 +0200

    Add lineChart

--- sportsee/src/component/averageSessions/AverageSessions.jsx
+++ sportsee/src/component/averageSessions/AverageSessions.jsx
@@ -1,10 +1,26 @@
 import './average.scss';
 
+import { ResponsiveContainer, LineChart, CartesianGrid, XAxis,YAxis, Tooltip, Line, Legend } from 'recharts';
+import { lineData } from '../../mock/dataMocked';
+
 const AverageSessions = () => {
+
+  const data = lineData;
 
   return (
     <div className="averageSessions">
-      AverageSessions
+      <ResponsiveContainer >
+        <LineChart width={730} height={250} data={data}
+        margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Line type="monotone" dataKey="pv" stroke="#8884d8" />
+          <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+        </LineChart>
+      </ResponsiveContainer>
     </div>
   )
 }
