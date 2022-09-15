2d329470e15cea1c6edd90cb37ccbc0db6d7fc84
JorisMLN <monleon.joris@gmail.com>
2022-09-15 15:24:40 +0200

    add score radial chart

--- sportsee/src/component/dailyScore/DailyScore.jsx
+++ sportsee/src/component/dailyScore/DailyScore.jsx
@@ -1,10 +1,29 @@
 import './score.scss';
 
+import { ResponsiveContainer, RadialBarChart, RadialBar, Tooltip, Legend } from 'recharts';
+import { scoreData } from '../../mock/dataMocked';
+
 const DailyScore = () => {
+
+  const data = scoreData;
 
   return (
     <div className="dailyScore">
-      DailyScore
+      <ResponsiveContainer >
+        <RadialBarChart 
+          width={730} 
+          height={250} 
+          innerRadius="10%" 
+          outerRadius="80%" 
+          data={data} 
+          startAngle={180} 
+          endAngle={0}
+        >
+          <RadialBar minAngle={15} label={{ fill: '#666', position: 'insideStart' }} background clockWise={true} dataKey='uv' />
+          <Legend iconSize={10} width={120} height={140} layout='vertical' verticalAlign='middle' align="right" />
+          <Tooltip />
+        </RadialBarChart>
+      </ResponsiveContainer>
     </div>
   )
 }
